b4c6ea7bcd8403893886858eebd80e6c6d3788df
Joseph <josephiusx@gmail.com>
2021-05-23 20:52:29 -0700

    added documentation to addTodo function

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Speech
+add button to todo that selects from filter categories
--- project/app.js
+++ project/app.js
@@ -1,3 +1,148 @@
+// todo app /////////////////////////////////////////////////
+// todo app /////////////////////////////////////////////////
+// todo app /////////////////////////////////////////////////
+
+// //Select DOM
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+//Event Listeners
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
+filterOption.addEventListener("click", filterTodo);
+
+//Functions
+
+function addTodo(e) { // pass in event to addTodo function
+  
+  e.preventDefault();//Prevent natural behaviour
+  
+  const todoDiv = document.createElement("div");//Create todo div
+  todoDiv.classList.add("todo"); // give it a class of todo
+  
+  const newTodo = document.createElement("li");//Create list
+  newTodo.innerText = todoInput.value; // give li todoInput(text area) value
+  
+  saveLocalTodos(todoInput.value);//Save to local - do this last
+
+  newTodo.classList.add("todo-item"); // add class todo-item to newTodo li item
+  todoDiv.appendChild(newTodo); // append new todo to 
+  todoInput.value = ""; // clear the text input value
+  
+  const completedButton = document.createElement("button");//Create Completed Button
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`; // give completed button inner html
+  completedButton.classList.add("complete-btn"); // give it a class
+  todoDiv.appendChild(completedButton); // append the button to todo div
+  
+  const trashButton = document.createElement("button");//Create trash button
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`; // give it an html 
+  trashButton.classList.add("trash-btn"); // give it a classlist
+  todoDiv.appendChild(trashButton); // append trashButton to todo-div created above
+  
+  todoList.appendChild(todoDiv); // append todoDiv to todo list
+}
+
+function deleteTodo(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "trash-btn") {
+    // e.target.parentElement.remove();
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    //at the end
+    removeLocalTodos(todo);
+    todo.addEventListener("transitionend", e => {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+    console.log(todo);
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes; // make a variable for all the todoList children (the todos)
+  todos.forEach(function(todo) { // for each todo in list todos
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function(todo) {
+    //Create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create list
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    todoInput.value = "";
+    //Create Completed Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Create trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //attach final Todo
+    todoList.appendChild(todoDiv);
+  });
+}
+
 // text to speach and buttons ///////////////////////////////
 // text to speach and buttons ///////////////////////////////
 // text to speach and buttons ///////////////////////////////
@@ -8,6 +153,7 @@ const stopButton = document.getElementById('stop-button')
 const textInput = document.getElementById('text')
 const speedInput = document.getElementById('speed')
 const clearButton = document.getElementById('clear-button')
+const container = document. querySelector('subject-container')
 let currentCharacter
 var synth = window.speechSynthesis;
 
@@ -113,8 +259,8 @@ const Keyboard = {
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
             "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
             "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "clear",
-            "space",'save', /*"pause"*/, "stop", "play"
+            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "save" , '',
+            ,'clear',"space" /*"pause"*/, "stop", "play"
         ];
 
         // Creates HTML for an icon
@@ -157,6 +303,10 @@ const Keyboard = {
                 case "save" :
                     keyElement.classList.add('keyboard__key--wide');
                     keyElement.textContent = key.toLocaleLowerCase();
+
+                    keyElement.addEventListener('click', () => {
+                        console.log(this.properties.value)
+                    })
                     
                     break;
 
@@ -288,3 +438,10 @@ const Keyboard = {
 window.addEventListener("DOMContentLoaded", function () {
     Keyboard.init();
 });
+
+const Category = function(subject, phrase) {
+    this.subject = subject;
+    this.phrase = phrase;
+}
+
+console.log(new Category('subject', 'phrase'))
